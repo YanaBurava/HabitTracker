4d52872c346d694bdf79812123363b097d857991
Yanina Burova <yanina.burova@betfred.com>
2025-06-15 12:34:55 +0200

    add achievements and stat

--- habit-tracker-app/src/app/pages/achievements/achievements.component.ts
+++ habit-tracker-app/src/app/pages/achievements/achievements.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Habit } from '../../models/habit.model';
+import { HabitsService } from '../../services/today-habit.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-achievements',
@@ -6,6 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './achievements.component.html',
   styleUrl: './achievements.component.scss'
 })
-export class AchievementsComponent {
+export class AchievementsComponent implements OnInit {
+  achievedHabits: Habit[] = [];
 
+  constructor(private habitsService: HabitsService, private router: Router) {}
+
+  ngOnInit(): void {
+    const allHabits = this.habitsService.getHabits();
+    this.achievedHabits = allHabits.filter(
+      habit => habit.progress.length >= habit.goal
+    );
+  }
+
+  goToHabitDetail(habit: Habit) {
+    this.router.navigate(['/habit', habit.id]);
+  }
 }
--- habit-tracker-app/src/app/pages/statistics/statistics.component.ts
+++ habit-tracker-app/src/app/pages/statistics/statistics.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges  } from '@angular/core';
+import { MOCK_HABIT } from '../../mock/mock-habit';
+import { Habit } from '../../models/habit.model';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-statistics',
@@ -6,6 +9,28 @@ import { Component } from '@angular/core';
   templateUrl: './statistics.component.html',
   styleUrl: './statistics.component.scss'
 })
-export class StatisticsComponent {
+export class StatisticsComponent implements OnInit {
+  calendarData: { date: string; count: number }[] = [];
+  endDate = new Date();
 
+habits: Habit[] = [];
+componentsOrder = ['radar', 'settings'];
+
+drop(event: CdkDragDrop<string[]>) {
+  moveItemInArray(this.componentsOrder, event.previousIndex, event.currentIndex);
+}
+  ngOnInit() {
+    this.habits = MOCK_HABIT;
+    const allDates = this.habits.flatMap(habit => habit.progress);
+
+    const dateCountMap: Record<string, number> = {};
+    for (const date of allDates) {
+      dateCountMap[date] = (dateCountMap[date] || 0) + 1;
+    }
+
+    this.calendarData = Object.entries(dateCountMap).map(([date, count]) => ({
+      date,
+      count
+    }));
+  }
 }
