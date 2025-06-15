883c804b5d122a86be8ee198910433f589ccfebc
Yanina Burova <yanina.burova@betfred.com>
2025-06-15 12:34:55 +0200

    add achievements and stat

--- habit-tracker-app/src/app/pages/statistics/statistics.module.ts
+++ habit-tracker-app/src/app/pages/statistics/statistics.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { StatisticsRoutingModule } from './statistics-routing.module';
 import { StatisticsComponent } from './statistics.component';
-
+import { SettingsComponent} from '../settings/settings.component';
+import { NgChartsModule } from 'ng2-charts';
+import { RadarChartComponent} from '../../components/radar-chart/radar-chart.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
-  declarations: [
-    StatisticsComponent
-  ],
+  declarations: [StatisticsComponent,  
+      SettingsComponent, 
+      RadarChartComponent
+],
   imports: [
     CommonModule,
-    StatisticsRoutingModule
-  ]
+    StatisticsRoutingModule,
+    NgChartsModule,
+    DragDropModule,
+      ],
+       exports: [RadarChartComponent]
 })
 export class StatisticsModule { }
