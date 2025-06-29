cafd5edb31219a3ddac5e2a074f4753734faae25
Yanina Burova <yanina.burova@betfred.com>
2025-06-29 21:23:55 +0200

    add progress

---
+++ habit-tracker-app/src/app/components/circular-progress/circular-progress/circular-progress.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-circular-progress',
+  templateUrl: './circular-progress.component.html',
+  styleUrls: ['./circular-progress.component.scss'],
+  standalone: false,
+})
+export class CircularProgressComponent implements OnChanges {
+  @Input() progress = 0; // процент 0..100
+
+  radius = 50;
+  circumference = 2 * Math.PI * this.radius;
+  strokeDashoffset = this.circumference;
+
+  ngOnChanges(): void {
+    const validProgress = Math.min(100, Math.max(0, this.progress));
+    this.strokeDashoffset = this.circumference - (this.circumference * validProgress) / 100;
+  }
+}
+
+
